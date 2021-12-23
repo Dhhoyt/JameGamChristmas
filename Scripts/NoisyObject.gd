4ccffc75ab56fbc3a4e93e3759b4ab8056b9490f
Dallin Hoyt <dhhoyt2002@gmail.com>
2021-12-23 15:42:54 -0700

    Start of noisy stuff

--- Scripts/NoisyObject.gd
+++ Scripts/NoisyObject.gd
@@ -1,6 +1,19 @@
 extends StaticBody
 
+export(AudioStream) var stream
+
 var playing = false
+
+func _ready():
+	$AudioStreamPlayer3D.stream = stream
+
+func play():
+	$AudioStreamPlayer3D.play()
+	playing = true
+	
+func stop():
+	$AudioStreamPlayer3D.stop()
+	playing = false
 
 func remind_krampus(krampus):
 	if playing:
