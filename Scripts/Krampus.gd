0b360cc11740ca6c871e17d0bbdde4edd7b0ab8d
Isaiah S <sirstotes@gmail.com>
2021-12-23 18:37:52 -0700

    Fix Krampus

--- Scripts/Krampus.gd
+++ Scripts/Krampus.gd
@@ -3,7 +3,8 @@ extends KinematicBody
 var path = []
 var path_node = 0
 
-var speed = 5
+var speed = 1
+var chase_speed = 3
 var hearing_dist = 5
 var fov = cos(PI/4)
 
@@ -25,6 +26,18 @@ func _process(delta):
 	if within_view():
 		state = 0
 		new_spot()
+	if state == 0:
+		$krampus/AnimationPlayer.play("Chase")
+		$krampus.look_at(player.global_transform.origin, Vector3.UP)
+		$krampus.rotation_degrees = Vector3(0, $krampus.rotation_degrees.y-90, 0)
+	elif state == 1 or state == 2:
+		$krampus/AnimationPlayer.play("Wander")
+		$krampus.look_at(path[path_node], Vector3.UP)
+		$krampus.rotation_degrees = Vector3(0, $krampus.rotation_degrees.y-90, 0)
+	else:
+		$krampus/AnimationPlayer.play("Stand")
+		$krampus.look_at(path[path_node], Vector3.UP)
+		$krampus.rotation_degrees = Vector3(0, $krampus.rotation_degrees.y-90, 0)
 
 func _physics_process(delta):
 	if path_node < path.size():
@@ -46,7 +59,7 @@ func _physics_process(delta):
 
 func move_to(target_pos):
 	path = nav.get_node("DetourNavigationMesh").find_path(global_transform.origin, target_pos)["points"]
-	print(randi())
+	#print(randi())
 	path_node = 0
 	
 func within_view():
