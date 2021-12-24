18e0d4f870bdd72e124884446d068d8f33888853
Isaiah S <sirstotes@gmail.com>
2021-12-24 00:08:36 -0700

    Populate Furniture

--- Scripts/Item.gd
+++ Scripts/Item.gd
@@ -2,7 +2,6 @@ extends Node
 class_name Item, "res://Assets/UI/Icons/flashlight.png"
 export var title = "Item"
 export var imgPath = "res://Assets/UI/Icons/flashlight.png"
-var img
 
-func _ready():
-	img = load(imgPath)
+func get_image():
+	return load(imgPath)
