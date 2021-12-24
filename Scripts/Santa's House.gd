ce8bac3da8022e3338a9ff17e92d65faa2c17125
Dallin Hoyt <dhhoyt2002@gmail.com>
2021-12-23 18:44:54 -0700

    Idk what happened lmao

--- Scripts/Santa's House.gd
+++ Scripts/Santa's House.gd
@@ -1,6 +1,8 @@
 extends Spatial
 
 var noiseies = []
+var chaseSounds = [preload("res://Assets/Audio/fxChase01.ogg"), preload("res://Assets/Audio/fxChase02.ogg"), preload("res://Assets/Audio/fxChase04.ogg")]
+var ambientSounds = [preload("res://Assets/Audio/ambientBells.ogg"), preload("res://Assets/Audio/ambientCarol01.ogg"), preload("res://Assets/Audio/ambientCarol01.ogg"), preload("res://Assets/Audio/ambientCarol02.ogg"), preload("res://Assets/Audio/ambientCarol03.ogg"), preload("res://Assets/Audio/ambientCarol04.ogg"), preload("res://Assets/Audio/ambientCarol05.ogg"), preload("res://Assets/Audio/ambientElf01.ogg"), preload("res://Assets/Audio/ambientGoat.ogg")]
 
 var krampus_turn_off_dist = 1
 
@@ -11,7 +13,20 @@ func add_noisy(new_noisy):
 func remove_noisy(new_noisy):
 	noiseies.erase(new_noisy)
 	new_noisy.play()
-	
+func _process(delta):
+	if $Krampus.state == 0:
+		$"Sound Effects/Ambient".stop()
+		if not $"Sound Effects/Chase".playing:
+			$"Sound Effects/Chase".stream = chaseSounds[randi()%len(chaseSounds)]
+			$"Sound Effects/Chase".play()
+	else:
+		if not $"Sound Effects/Chase".playing:
+			if not $"Sound Effects/Ambient".playing:
+				if $"Sound Effects/AmbientTimer".time_left <= 0:
+					$"Sound Effects/AmbientTimer".start(randi()%3)
+func play_ambient():
+	$"Sound Effects/Ambient".stream = ambientSounds[randi()%len(ambientSounds)]
+	$"Sound Effects/Ambient".play()
 func _physics_process(delta):
 	for i in noiseies:
 		if i.global_transform.origin.distance_to($Krampus.global_transform.origin) < krampus_turn_off_dist:
