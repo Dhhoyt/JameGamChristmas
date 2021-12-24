8a9e1d3315f6c6a23a6f039621fd79f72d071ad6
Dallin Hoyt <dhhoyt2002@gmail.com>
2021-12-24 02:00:00 -0700

    Cutscene

--- Scripts/WinState.gd
+++ Scripts/WinState.gd
@@ -19,16 +19,22 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	$Krampus/krampus/AnimationPlayer.play()
+	$krampus/AnimationPlayer.play("Stand")
+	time += delta
 	if playing:
-		time += delta
 		if time >= 1:
 			falling = true
 		if falling:
 			velocity += gravity_vector * gravity_magnitude * delta * 0.5
 			chandelier.transform.origin += velocity * delta * 0.5
 		if chandelier.transform.origin.y <= 2:
+			time = 0
 			playing = false
 			chandelier.queue_free()
 			$AudioStreamPlayer.play()
 			$Camera2.current = true
+	else:
+		if time >= 4:
+			$CanvasLayer/Label.visible = true
+		if time >= 10:
+			get_tree().change_scene("res://Scenes/TitleScreen.tscn")
